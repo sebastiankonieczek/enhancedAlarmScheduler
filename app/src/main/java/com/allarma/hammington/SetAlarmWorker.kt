cea1ebb9fd809d1eadc1baf5ed29856ae8d699f6
Sebastian Konieczek <sebastian.konieczek@openvalue.de>
2021-08-30 22:23:19 +0200

    Use alarm manager for scheduling alarms
    as starting an activity with an intent from background does not work,
    the set alarm intent is not useful, even though it would have been awesome to use androids
    internal clock app for scheduling alarms!

--- app/src/main/java/com/allarma/hammington/SetAlarmWorker.kt
+++ app/src/main/java/com/allarma/hammington/SetAlarmWorker.kt
@@ -1,26 +1,45 @@
 package com.allarma.hammington
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Handler
-import android.os.Looper
-import android.provider.AlarmClock
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.allarma.hammington.activities.AlarmSchedulerService
+import com.allarma.hammington.activities.AlarmAction
+import com.allarma.hammington.activities.AlarmProfileOverviewActivity
 import com.allarma.hammington.database.AppDatabase
 import com.allarma.hammington.model.Alarm
 import com.allarma.hammington.model.AlarmProfileWithAlarms
 import kotlinx.coroutines.*
 import java.time.Duration
 import java.time.LocalDate
-import java.util.stream.Collectors
+import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 class SetAlarmWorker(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams), CoroutineScope {
+
+   private val contentIntent by lazy {
+      PendingIntent.getActivity(
+         this.applicationContext,
+         0,
+         Intent(this.applicationContext, AlarmProfileOverviewActivity::class.java),
+         PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+      )
+   }
+
+
    override fun doWork(): Result {
+      val alarmManager =
+         applicationContext.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+
+      val broadcast = PendingIntent.getBroadcast(
+         applicationContext,
+         0,
+         Intent(applicationContext, AlarmAction::class.java),
+         PendingIntent.FLAG_IMMUTABLE
+      )
+      alarmManager?.cancel(broadcast)
 
       AppDatabase.invoke(applicationContext)
          .dao()
@@ -32,18 +51,22 @@ class SetAlarmWorker(appContext: Context, workerParams: WorkerParameters) : Work
          .filter { alarm -> alarm.frequency_ != Alarm.Frequency.UNKNOWN }
          .filter(this::isAlarmRelevant)
          .forEach { alarm ->
-            runBlocking {
-               launch {
-                  val intent =
-                     Intent(applicationContext, AlarmSchedulerService::class.java)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        .addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-                        .putExtra("hour", alarm.getHour())
-                        .putExtra("minute", alarm.getMinute())
-                  ContextCompat.startForegroundService(applicationContext, intent)
-                  delay(200)
-               }
+            val hour = alarm.getHour()
+            val minute = alarm.getMinute()
+
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, hour!!)
+            calendar.set(Calendar.MINUTE, minute!!)
+
+            if(calendar.before(Calendar.getInstance())){
+               calendar.add(Calendar.DATE, 1)
             }
+            val toEpochSecond = calendar.timeInMillis
+
+            println( "set alarm $hour:$minute" )
+
+            val alarmClockInfo = AlarmManager.AlarmClockInfo(toEpochSecond, contentIntent)
+            alarmManager?.setAlarmClock(alarmClockInfo, broadcast)
          }
       return Result.success()
    }
